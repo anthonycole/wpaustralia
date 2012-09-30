c657ddeb446279b37ee51a174771768b7c60a6a1
Bronson Quick <bronson@sennza.com.au>
2012-09-30 18:30:33 +1000

    Turns out WordPress SEO was breaking TinyMCE in bbPress. I haven't looked into why it happens but I have deactivated WordPress SEO and altered the <title> tags to fix #13.

--- themes/wpaustralia/header.php
+++ themes/wpaustralia/header.php
@@ -4,7 +4,27 @@
 	<head profile="http://gmpg.org/xfn/11">
 		<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
 		<?php if ( current_theme_supports( 'bp-default-responsive' ) ) : ?><meta name="viewport" content="width=device-width, initial-scale=1.0" /><?php endif; ?>
-		<title><?php wp_title( '' ); ?></title>
+		<title><?php
+			/*
+				 * Print the <title> tag based on what is being viewed.
+				 */
+			global $page, $paged;
+
+			wp_title( '|', true, 'right' );
+
+			// Add the blog name.
+			bloginfo( 'name' );
+
+			// Add the blog description for the home/front page.
+			$site_description = get_bloginfo( 'description', 'display' );
+			if ( $site_description && ( is_home() || is_front_page() ) )
+				echo " | $site_description";
+
+			// Add a page number if necessary:
+			if ( $paged >= 2 || $page >= 2 )
+				echo ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
+
+			?></title>
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
 		<?php do_action( 'bp_head' ); ?>
