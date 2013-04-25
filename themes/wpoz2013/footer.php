ea118331dfd324fd9a697dd7415e9156a3956321
Bronson Quick <bronson@sennza.com.au>
2013-04-25 22:21:24 +1000

    Time to commit before WordCamp Melb

---
+++ themes/wpoz2013/footer.php
@@ -0,0 +1,23 @@
+<div class="clear"></div>
+</div> <!-- #container -->		
+
+		<?php do_action( 'bp_after_container' ); ?>
+
+</div><!-- main -->
+
+		<?php do_action( 'bp_before_footer'   ); ?>
+
+	
+<footer>
+	
+Proudly powered by <a href="http://www.wordpress.org" title="WordPress">WordPress</a> and <a href="http://www.buddypress.org" title="BuddyPress">BuddyPress</a>
+
+</footer>
+
+<?php do_action( 'bp_after_footer' ); ?>
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
---
+++ themes/wpoz2013/functions.php
@@ -0,0 +1,49 @@
+<?php /**
+ * Returns the Google font stylesheet URL if available.
+ *
+ * The use of Open Sans by default is localized. For languages that use
+ * characters not supported by the font, the font can be disabled.
+ *
+ * @return string Font stylesheet or empty string if disabled.
+ */
+function wpoz_get_font_url() {
+	$font_url = '';
+
+	/* translators: If there are characters in your language that are not supported
+	 by Open Sans, translate this to 'off'. Do not translate into your own language. */
+	if ( 'off' !== _x( 'on', 'Open Sans font: on or off', 'wpoz' ) ) {
+		$subsets = 'latin,latin-ext';
+
+		/* translators: To add an additional Open Sans character subset specific to your language, translate
+		 this to 'greek', 'cyrillic' or 'vietnamese'. Do not translate into your own language. */
+		$subset = _x( 'no-subset', 'Open Sans font: add new subset (greek, cyrillic, vietnamese)', 'wpoz' );
+
+		if ( 'cyrillic' == $subset )
+			$subsets .= ',cyrillic,cyrillic-ext';
+		elseif ( 'greek' == $subset )
+			$subsets .= ',greek,greek-ext';
+		elseif ( 'vietnamese' == $subset )
+			$subsets .= ',vietnamese';
+
+		$protocol = is_ssl() ? 'https' : 'http';
+		$query_args = array(
+			'family' => 'Open+Sans:400italic,700italic,400,700',
+			'subset' => $subsets,
+		);
+		$font_url = add_query_arg( $query_args, "$protocol://fonts.googleapis.com/css" );
+	}
+
+	return $font_url;
+}
+/**
+ * Enqueues scripts and styles for front-end.
+ *
+ */
+function wpoz_scripts_styles() {
+	
+	$font_url = wpoz_get_font_url();
+	if ( ! empty( $font_url ) )
+		wp_enqueue_style( 'wpoz-fonts', esc_url_raw( $font_url ), array(), null );
+
+}
+add_action( 'wp_enqueue_scripts', 'wpoz_scripts_styles' );
